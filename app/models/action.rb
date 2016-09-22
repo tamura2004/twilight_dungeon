b7981a44be04a7cd3313a5f03e376d31439ea091
tamura <tamura2004@gmail.com>
2016-09-22 11:00:44 +0900

    fire

--- app/models/action.rb
+++ app/models/action.rb
@@ -5,13 +5,77 @@ class Action < ApplicationRecord
   def run(d20:nil)
     d20 ||= rand(20)+1
 
-    case name
-    when "攻撃"
-      self.message = source.attack(target,d20)
-    else
+    DungeonMaster.new(self) do |dm|
+      dm.perform!
+      self.message = dm.message
     end
 
-    self.message += source.victory?(target)
   end
+
+end
+
+class DungeonMaster
+  delegate :perform!, :message, to: :rule_book
+
+  def initialize(action)
+    case action.name
+    when "攻撃"
+      @rule_book = DungeonMastersGuild::Attack.new(action)
+    end
+  end
+end
+
+class DungeonMastersGuild::Attack
+  delegate :perform!, :message, to: :rule_book
+
+  def initialize(action)
+    case @attacker.type
+    when Player
+      @rule_book = PlayerHandbook::Attack.new(action)
+    when Monster
+      @rule_book = MonsterManual::Attack.new(action)
+    end
+  end
+end
+
+class PlayerHandbook::Attack
+  attr_reader :message
+
+  def initialize(action)
+    @player = action.source
+    @monster = action.target
+  end
+
+  def log(message)
+    @message += message + "\n"
+  end
+
+  def attack!
+    log "#{@player}は#{@monster}を攻撃。"
+    Dice.roll do |d20|
+
+    case Dice.roll
+    when :critical then critical_hit!
+    when :fumble then fumble!
+    else normal_damage(enemy,d20)
+    end
+  end
+
+  def normal_attack(enemy,d20)
+    damage = (at*d20/10 - enemy.df)
+    damage = d20 if damage.zero?
+    enemy.damaged(damage)
+    "#{enemy.name}に#{damage}ダメージ。\n"
+  end
+
+  def critical(enemy)
+    enemy.update(hp:0)
+    "クリティカルヒット！\n"
+  end
+
+  def fumble(enemy)
+    "ファンブル！\n"
+  end
+
 
 end
--- app/models/creature.rb
+++ app/models/creature.rb
@@ -1,29 +1,7 @@
 class Creature < ApplicationRecord
 
-  def attack(enemy,d20)
-    "#{name}は#{enemy.name}を攻撃。d20->#{d20}\n" +
-    case d20
-    when 20 then critical(enemy)
-    when 1 then fumble(enemy)
-    else normal_attack(enemy,d20)
-    end
-  end
-
-  def normal_attack(enemy,d20)
-    damage = (at*d20/10 - enemy.df)
-    damage = d20 if damage.zero?
-    enemy.damaged(damage)
-    "#{enemy.name}に#{damage}ダメージ。\n"
-  end
-
-  def critical(enemy)
-    enemy.update(hp:0)
-    "クリティカルヒット！\n"
-  end
-
-  def fumble(enemy)
-    "ファンブル！\n"
-  end
+  scope :lived, -> {where("hp > 0")}
+  scope :dead, -> {where(hp: 0)}
 
   def damaged(damage)
     if hp < damage
@@ -41,7 +19,8 @@ class Creature < ApplicationRecord
     hp == 0
   end
 
-  scope :lived, -> {where("hp > 0")}
-  scope :dead, -> {where(hp: 0)}
+  def to_s
+    name
+  end
 
 end
